af972b467f3ac9414a4718373d1eec7b79729bfd
Benjamin Bolte <ben@bolte.cc>
2024-06-10 23:49:42 -0700

    format

--- frontend/src/components/auth/RequireAuthentication.tsx
+++ frontend/src/components/auth/RequireAuthentication.tsx
@@ -6,23 +6,6 @@ interface Props {
   children: React.ReactNode;
 }
 
-const StaticBackground = () => {
-  return (
-    <div
-      style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        zIndex: -1,
-        width: "100vw",
-        height: "100vh",
-        backgroundColor: "black",
-        opacity: 0.5,
-      }}
-    />
-  );
-};
-
 const RequireAuthentication = (props: Props) => {
   const { children } = props;
   const { isAuthenticated } = useAuthentication();
