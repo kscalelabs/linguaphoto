2f195abbb5c690607054a31e3a5bd96291336c07
Benjamin Bolte <ben@bolte.cc>
2024-06-16 10:21:41 -0700

    fix tests

--- linguaphoto/main.py
+++ linguaphoto/main.py
@@ -4,8 +4,6 @@ from fastapi import FastAPI, Request, status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 
-from linguaphoto.routers.part import parts_router
-from linguaphoto.routers.robot import robots_router
 from linguaphoto.routers.users import users_router
 from linguaphoto.settings import settings
 
@@ -30,5 +28,3 @@ async def value_error_exception_handler(request: Request, exc: ValueError) -> JS
 
 
 app.include_router(users_router, prefix="/users", tags=["users"])
-app.include_router(robots_router, prefix="/robots", tags=["robots"])
-app.include_router(parts_router, prefix="/parts", tags=["parts"])
